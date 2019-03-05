6c6ff48f0dc40458086c13087eb28c60ccf143b7
nurbekiii <36946211+nurbekiii@users.noreply.github.com>
2019-03-05 18:21:27 +0600

    Add files via upload

---
+++ src/main/java/com/test/hello/Main.java
@@ -0,0 +1,12 @@
+package com.test.hello;
+
+/**
+ * @author NIsaev on 05.03.2019
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world11");
+    }
+
+}
