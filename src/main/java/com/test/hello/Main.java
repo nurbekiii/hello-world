7297d97cc22263d1459637514c359a889ee829de
NIsaev <nurbekiii@mail.ru>
2019-03-05 18:37:26 +0600

    initial commit

--- src/main/java/com/test/hello/Main.java
+++ src/main/java/com/test/hello/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        if(true) {
+        if(true==) {
             System.out.println("Hello world2222");
             System.out.println("Hello world2222");
             System.out.println("Hello world2222");
